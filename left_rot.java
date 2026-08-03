cf1a98e211c6939d7c06144aa2656181d8746fd6
jenishamary896 <jenishamary896@gmail.com>
2026-08-03 16:02:09 +0530

    Update left_rot.java

--- left_rot.java
+++ left_rot.java
@@ -24,3 +24,44 @@ class Main {
         }
     }
 }
+
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int rot=sc.nextInt();
+        for(int i=n-rot;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-rot;i++){
+            arr[i]=sc.nextInt();
+        }
+          
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}//rotating while getting input itself
+
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int rot=sc.nextInt();
+        for(int i=0;i<n;i++){
+            arr[(i+n-rot)%n]=sc.nextInt();
+        }
+        
+          
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}//single for loop
+
