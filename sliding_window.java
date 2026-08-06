29052f47430f0fc166ec5e432c819ca80966cee6
jenishamary896 <jenishamary896@gmail.com>
2026-08-06 16:48:52 +0530

    Implement sliding window functionality in Java

---
+++ sliding_window.java
@@ -0,0 +1,18 @@
+mport java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int k =sc.nextInt();
+        for(int j=0;j<=(n-k);j++){
+        for(int i=j;i<(j+k);i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        }
+    }
+}
